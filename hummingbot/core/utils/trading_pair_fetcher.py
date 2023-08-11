21ea7583123b8a5b8d0d8735a074df392095b40a
Ajayi israel <isreallee82@gmail.com>
2023-08-11 08:21:18 +0000

    feat/fetch from filtered info

--- hummingbot/core/utils/trading_pair_fetcher.py
+++ hummingbot/core/utils/trading_pair_fetcher.py
@@ -7,7 +7,18 @@ from hummingbot.logger import HummingbotLogger
 
 from .async_utils import safe_ensure_future
 
+"""
+        Add a new config variable to conf_client.yml: fetch_pairs_from_all_exchanges: False.
 
+        Change the trading pair fetcher so that 
+            if fetch_pairs_from_all_exchanges  is False, 
+                only fetches pairs from exchanges where the user has added an API key, && the exchanges in paper_trade_exchanges in the conf_client.yml
+            elseif True, 
+                the fetcher operates as it does now and fetches pairs from all exchange connectors
+                
+        After a user added API key with connect, 
+            fetch pairs from that new exchange
+"""
 class TradingPairFetcher:
     _sf_shared_instance: "TradingPairFetcher" = None
     _tpf_logger: Optional[HummingbotLogger] = None
@@ -28,6 +39,8 @@ class TradingPairFetcher:
     def __init__(self, client_config_map: ClientConfigAdapter):
         self.ready = False
         self.trading_pairs: Dict[str, Any] = {}
+        self._fetch_pairs_from_all_exchanges = client_config_map.fetch_pairs_from_all_exchanges
+        self.paper_trades_in_conf = client_config_map.paper_trade.paper_trade_exchanges
         self._fetch_task = safe_ensure_future(self.fetch_all(client_config_map))
 
     def _fetch_pairs_from_connector_setting(
@@ -38,34 +51,42 @@ class TradingPairFetcher:
         connector = connector_setting.non_trading_connector_instance_with_default_configuration()
         safe_ensure_future(self.call_fetch_pairs(connector.all_trading_pairs(), connector_name))
 
+    def _fetch_from_filtered(
+            self,
+            connector_setting: ConnectorSetting,
+            conn_setting: AllConnectorSettings.get_connector_settings(),
+            conn_name: Optional[str] = None,
+            connector_name: Optional[str] = None):
+        conn_name = conn_name or conn_setting.name
+        connector_name = connector_name or connector_setting.name
+        connector = connector_setting.non_trading_connector_instance_with_default_configuration()
+        safe_ensure_future(self.call_fetch_pairs(connector.all_trading_pairs(), conn_name))
+
     async def fetch_all(self, client_config_map: ClientConfigAdapter):
         connector_settings = self._all_connector_settings()
-        # print(f"{connector_settings}")
         for conn_setting in connector_settings.values():
-            # XXX(martin_kou): Some connectors, e.g. uniswap v3, aren't completed yet. Ignore if you can't find the
-            # data source module for them.
+             # XXX(martin_kou): Some connectors, e.g. uniswap v3, aren't completed yet. Ignore if you can't find the
+             # data source module for them.
             try:
-                if conn_setting.base_name().endswith("paper_trade"):
-                    self._fetch_pairs_from_connector_setting(
-                        connector_setting=connector_settings[conn_setting.parent_name],
-                        connector_name=conn_setting.name
-                    )
-                    # print("basename endswith paper_trade")
-                    # print(" ******************************* ")
-                    # print(f"{conn_setting}")
-                    # print(" ******************************* ")
+                if self._fetch_pairs_from_all_exchanges:
+                    if 'api_keys' in conn_setting.config_keys or conn_setting.base_name().endswith("paper_trade"):
+                        self._fetch_from_filtered(
+                            connector_setting=connector_settings[conn_setting.parent_name],
+                            connector_name=conn_setting.name
+                        )
                 else:
-                    self._fetch_pairs_from_connector_setting(connector_setting=conn_setting)
-                    # print("basename")
-                    # print(" ******************************* ")
-                    # print(f"{conn_setting}")
-                    # print(" ******************************* ")
+                    if conn_setting.base_name().endswith("paper_trade"):
+                        self._fetch_pairs_from_connector_setting(
+                            connector_setting=connector_settings[conn_setting.parent_name],
+                            connector_name=conn_setting.name
+                        )
+                    else:
+                        self._fetch_pairs_from_connector_setting(connector_setting=conn_setting)
             except ModuleNotFoundError:
                 continue
             except Exception:
-                self.logger().exception(f"An error occurred when fetching trading pairs for {conn_setting.name}."
+                self.logger().exception(f"An error occurred when fetching trading pairs for {conn_setting.name}. "
                                         "Please check the logs")
-
         self.ready = True
 
     async def call_fetch_pairs(self, fetch_fn: Callable[[], Awaitable[List[str]]], exchange_name: str):
