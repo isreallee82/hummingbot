54ea5dcd003f142eae417b2054f1903a8a923d62
isreallee82 <isreallee82@gmail.com>
2025-03-24 10:55:54 +0100

    (feat) Update derive signing method

--- hummingbot/connector/exchange/derive/derive_exchange.py
+++ hummingbot/connector/exchange/derive/derive_exchange.py
@@ -74,7 +74,7 @@ class DeriveExchange(ExchangePyBase):
 
     @property
     def authenticator(self) -> DeriveAuth:
-        return DeriveAuth(self.derive_api_key, self.derive_secret_key, self._sub_id, self._trading_required)
+        return DeriveAuth(self.derive_api_key, self.derive_secret_key, self._sub_id, self._trading_required, domain=self.name)
 
     @property
     def rate_limits_rules(self) -> List[RateLimit]:
