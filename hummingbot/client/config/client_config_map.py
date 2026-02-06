fff3b3eb4c73afe148e0f27be9aaab96b96e7621
isreallee82 <isreallee82@gmail.com>
2026-02-06 08:36:26 +0100

    feat: Add Evedex perpetual rate source mode to rate source configurations

--- hummingbot/client/config/client_config_map.py
+++ hummingbot/client/config/client_config_map.py
@@ -636,6 +636,11 @@ class EvedexRateSourceMode(ExchangeRateSourceModeBase):
     model_config = ConfigDict(title="evedex")
 
 
+class EvedexPerpetualRateSourceMode(ExchangeRateSourceModeBase):
+    name: str = Field(default="evedex_perpetual")
+    model_config = ConfigDict(title="evedex_perpetual")
+
+
 class CoinbaseAdvancedTradeRateSourceMode(ExchangeRateSourceModeBase):
     name: str = Field(default="coinbase_advanced_trade")
     model_config = ConfigDict(title="coinbase_advanced_trade")
@@ -675,6 +680,7 @@ RATE_SOURCE_MODES = {
     CoinCapRateSourceMode.model_config["title"]: CoinCapRateSourceMode,
     DexalotRateSourceMode.model_config["title"]: DexalotRateSourceMode,
     EvedexRateSourceMode.model_config["title"]: EvedexRateSourceMode,
+    EvedexPerpetualRateSourceMode.model_config["title"]: EvedexPerpetualRateSourceMode,
     KuCoinRateSourceMode.model_config["title"]: KuCoinRateSourceMode,
     GateIoRateSourceMode.model_config["title"]: GateIoRateSourceMode,
     CoinbaseAdvancedTradeRateSourceMode.model_config["title"]: CoinbaseAdvancedTradeRateSourceMode,
